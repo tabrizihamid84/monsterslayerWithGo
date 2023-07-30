a0e505abaaa7f2bd6816009799581bed326e75af
tabrizihamid84 <tabrizihamid6367@gmail.com>
2023-07-30 09:43:17 +0330

    add greeting func

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"github.com/tabrizihamid84/monsterSlayer/interaction"
+)
 
+func main() {
+	startGame()
+
+	winner := "" // "Player" || "Monster" || ""
+
+	for winner == "" {
+		winner = executeRound()
+	}
+
+	endGame()
 }
+
+func startGame() {
+	interaction.PrintGreeting()
+}
+
+func executeRound() string {
+	return ""
+}
+
+func endGame() {}
